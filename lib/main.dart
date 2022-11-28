70cfc109c65a9de0558e333842bbd45e55defa63
Ariz-99 <82720584+Ariz-99@users.noreply.github.com>
2022-11-28 20:15:04 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:my_grocery/model/ad_banner.dart';
+import 'package:my_grocery/model/category.dart';
+import 'package:my_grocery/model/product.dart';
+import 'package:my_grocery/route/app_page.dart';
+import 'package:my_grocery/route/app_route.dart';
+import 'package:my_grocery/theme/app.theme.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+
+  //register adapters
+  Hive.registerAdapter(AdBannerAdapter());
+  Hive.registerAdapter(CategoryAdapter());
+  Hive.registerAdapter(ProductAdapter());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      getPages: AppPage.list,
+      initialRoute: AppRoute.dashboard,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
+
